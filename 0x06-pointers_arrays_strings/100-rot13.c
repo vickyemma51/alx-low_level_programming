aae25e0884126fdaa695aa03d740bb2a3d8f55f6
vickyemma51 <vickyemma51@yahoo.com>
2023-03-02 20:47:08 +0000

    Task

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,6 +10,7 @@ char *rot13(char *s)
 	int count = 0, i;
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 52; i++)

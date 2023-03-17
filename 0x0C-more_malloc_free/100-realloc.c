8c4899799a852b94c5a23ff4ea438efe10a92325
vickyemma51 <vickyemma51@yahoo.com>
2023-03-17 21:13:56 +0000

    Task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,4 +1,4 @@
-#include "mainn.h"
+#include "main.h"
 #include <stdlib.h>
 /**
 * _realloc - reallocates a memory block.
@@ -25,4 +25,3 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	ptr = malloc(new_size);
 	return (ptr);
 }
-Footer

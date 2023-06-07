04120beb8d5f315f446d975fb5af79a8c63164c5
vickyemma51 <vickyemma51@yahoo.com>
2023-06-07 21:51:20 +0000

    Task 1 corrected

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* _print_rev_recursion - prints a string in reverse.
-
-
+* _print_rev_recursion - prints a string in reverse
+* @s: String
+* Return: no return.
 */
 void _print_rev_recursion(char *s)
 {

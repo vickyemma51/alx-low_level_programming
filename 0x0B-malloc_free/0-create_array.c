772177df80ad5a69342734ce1f4a366cca81c7be
vickyemma51 <vickyemma51@yahoo.com>
2023-06-13 21:53:28 +0000

    Task 0 correctioon

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,9 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
-	s = malloc((size + 1) * sizeof(char));
+	if (size == 0)
+		return (NULL);
+	s = malloc((size) * sizeof(char));
 	if (s == NULL)
 	{
 		return (NULL);

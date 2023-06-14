3aa6e6987c32ccb834bc87581fb480a7c5cc704a
vickyemma51 <vickyemma51@yahoo.com>
2023-06-14 21:54:05 +0000

    Task 4 correction

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -14,8 +14,10 @@ void free_grid(int **grid, int height)
 	if (grid != NULL && height != 0)
 	{
 		while (height >= 0)
+		{
+			free(grid[height]);
 			height--;
-		free(grid[height]);
+		}
 		free(grid);
 	}
 }

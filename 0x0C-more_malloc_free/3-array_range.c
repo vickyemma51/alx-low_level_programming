b7dbe87b548ecd075720c8be079ba136e8796306
vickyemma51 <vickyemma51@yahoo.com>
2023-06-16 13:49:24 +0000

    Task 3 correction

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -21,7 +21,8 @@ int *array_range(int min, int max)
 	a = malloc(((max - min) + 1) * sizeof(int));
 	if (a == NULL)
 		return (NULL);
+
 	for (i = 0; min <= max; min++, i++)
-			a[i] = min;
+		a[i] = min;
 	return (a);
 }

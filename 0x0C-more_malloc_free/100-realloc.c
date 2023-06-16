c2b630eba953bb59cbdc410f9720b85c5b3189eb
vickyemma51 <vickyemma51@yahoo.com>
2023-06-16 21:13:20 +0000

    Task 100 corrected

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,9 +20,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 
 	if ((new_size == 0) && ptr != NULL)
-	{	
+	{
 		free(ptr);
 		return (NULL);
+	}
 
 	a = malloc((old_size + new_size) * sizeof(int *));
 

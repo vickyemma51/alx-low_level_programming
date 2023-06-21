aece2831d563f9d50e1db9f9f012ad9f3c1dd1c2
vickyemma51 <vickyemma51@yahoo.com>
2023-06-21 22:00:21 +0000

    Function_pointer project

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: Name to print
+ * @f: pointer to a function
+ *
+ * Return: nothing to return
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer.
+ * @array: members of array
+ * @size: number of element in array
+ * @cmp: pointer to a function
+ *
+ * Return: -1 if no elements matches
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int j = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if ((cmp(array[i])) > 0)
+		{
+			j++;
+			break;
+		}
+	}
+	if (j != 0)
+		return (i);
+	else
+		return (-1);
+}
